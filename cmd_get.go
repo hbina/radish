7402bc833c7c4117d6b1a77bbc372bc1ae377a17
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2022-11-16 21:29:11 +0800

    Unwrapped genericGetCommand helper function
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- cmd_get.go
+++ cmd_get.go
@@ -1,36 +1,30 @@
 package redis
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func GetCommand(c *Client, args [][]byte) {
-	GetCommandRaw(c, args)
-}
-
-func GetCommandRaw(c *Client, args [][]byte) bool {
 	if len(args) == 0 {
 		c.Conn().WriteError("no argument passed to handler. This should not be possible")
-		return false
+		return
 	} else if len(args) == 1 {
 		c.Conn().WriteError(fmt.Sprintf(WrongNumOfArgsErr, args[0]))
-		return false
+		return
 	}
 
 	key := string(args[1])
-
 	item, _ := c.Db().GetOrExpire(key, true)
+
 	if item == nil {
 		c.Conn().WriteNull()
-		return true
+		return
 	}
 
 	if item.Type() == ValueTypeString {
 		v := item.Value().(string)
 		c.Conn().WriteBulkString(v)
-		return true
+		return
 	} else {
 		c.Conn().WriteError(fmt.Sprintf("%s: key is a %s not a %s", WrongTypeErr, item.TypeFancy(), ValueTypeFancyString))
-		return false
+		return
 	}
 }
