a5abbe2bdf60b85b7785e3ec1309eb4a514ff5b7
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2022-12-05 19:08:43 +0800

    Added set operations methods to ZSET and conversion from ZSET <-> SET
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- t_set.go
+++ t_set.go
@@ -18,6 +18,8 @@ func NewSetEmpty() *Set {
 	return &Set{inner: map[string]struct{}{}}
 }
 
+/// impl Item for Set
+
 func (s *Set) Value() interface{} {
 	return s.inner
 }
@@ -138,4 +140,14 @@ func (s *Set) ForEachF(f func(a string)) {
 	for k := range s.inner {
 		f(k)
 	}
+}
+
+func (s *Set) ToZSet() *ZSet {
+	set := NewZSet()
+
+	for k := range s.inner {
+		set.inner.AddOrUpdate(k, 1)
+	}
+
+	return set
 }
